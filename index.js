3a521c966ea979867527270660a2ddae7a87b4a6
sergeybarzul <96749365+sergeycodes@users.noreply.github.com>
2022-06-02 16:55:19 -0700

    created a page that has a flashlight as cursor with js to switch background color

---
+++ index.js
@@ -0,0 +1,27 @@
+
+document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty (
+        '--x', (
+            e.clientX+window.scrollX
+        )
+        + 'px'
+    );
+    document.documentElement.style.setProperty (
+        '--y', (
+            e.clientY+window.scrollY
+        )
+        + 'px'
+    );
+}
+
+
+function lightswitch() {
+    let light = document.querySelector(".showcase")
+    light.style.backgroundColor = "#fff"    
+    changeCursor()
+}
+
+function changeCursor() {
+    document.getElementById("cursor").setAttribute("id", "pointercursor")
+    document.getElementById("nocursor").setAttribute("id", "pointercursor")
+}
